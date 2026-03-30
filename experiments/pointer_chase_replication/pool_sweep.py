40697277f27ff4e38e36a5c0c78ac36b24b09062
kevins19 <kevin.j.shan@gmail.com>
2026-03-30 09:38:41 -0400

    Use 500 warmup and measured instructions

--- experiments/pointer_chase_replication/pool_sweep.py
+++ experiments/pointer_chase_replication/pool_sweep.py
@@ -32,8 +32,8 @@ MIN_RETIRED_BEFORE_LATENCY_CUTOFF = int(os.environ.get("MIN_RETIRED_BEFORE_LATEN
 # Output and run length
 LIGHTWEIGHT_OUTPUT = 1
 PRINT_LAT_HIST = 1
-WARMUP_INSTS = 300
-SIM_INSTS = 300
+WARMUP_INSTS = 500
+SIM_INSTS = 500
 
 sst.setProgramOption("partitioner", "sst.self")
 
